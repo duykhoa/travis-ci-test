ffe18cc73abaee6cc4dddef35f6b47d1a4404646
ITviec <lamphuong+khoa@itviec.com>
2014-07-02 11:34:23 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TravisCiTest::Application.config.secret_key_base = '357643f77a27739f1677023aab390cd5e8a5d64c3b625cc94c067bf36aff0f68fa2907771d4c390610e964a5a21b8573c91aeb3eef10fc79c3b881ceffa04c02'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TravisCiTest::Application.config.session_store :cookie_store, key: '_travis-ci-test_session'
